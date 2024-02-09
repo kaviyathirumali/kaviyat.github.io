cca696fba61586871dbd7d59b55a1a0e3215e5f8
kaviyathirumali <kaviyathiru007@gmail.com>
2024-02-10 00:02:54 +0530

    Updated website with changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes  } from "react-router-dom"; 
 import Navbar from "./Components/Navbar/Navbar";
 import Navlist from "./Components/Navbar-list/Navlist";
 
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
+      <Router>
         <Navbar />
         <Routes>
-           <Route path="/" element={<Navlist/>}/>
-        </Routes> 
-      </BrowserRouter>
-    </div>
+          <Route exact path="/kaviyat.github.io" Component={Navlist}/>
+        </Routes>
+      </Router>
   );
 }
 
