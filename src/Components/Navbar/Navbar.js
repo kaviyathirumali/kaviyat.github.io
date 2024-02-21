53d5c4c9727bb69067992b3d72181f4bf32493ef
kaviyathirumali <kaviyathiru007@gmail.com>
2024-02-21 10:31:17 +0530

    Updated website with changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import './Navbar.css';
 import { MdMenu} from 'react-icons/md'
 import {RxCross2} from 'react-icons/rx';
-import resume from '../../Assets/CV_2024013020290365.pdf'
 
 const Navbar = () => {
   const [menu,setMenu]= useState("Home")
@@ -12,15 +11,14 @@ const Navbar = () => {
   return (
     <div className='profile'>
         <div  className='navbar'>
-            <ul className={isMobile? "nav-link" : "nav-link-mobile"} onClick={()=>setIsMobile(false)} > 
+            <ul className={isMobile?  "nav-link-mobile" : "nav-link" } onClick={()=>setIsMobile(false)} > 
               <li onClick={()=>setMenu("Home")}><a href='#home'>Home</a>{menu==="Home"?<hr/>:<></>}</li>
               <li onClick={()=>setMenu("About")}><a href='#about'>About</a>{menu==="About"?<hr/>:<></>}</li>
               <li onClick={()=>setMenu("Skills")}><a href='#skills'>Skills</a>{menu==="Skills"?<hr/>:<></>}</li>
               <li onClick={()=>setMenu("Project")}><a href='#project'>Project</a>{menu==="Project"?<hr/>:<></>}</li>
               <li onClick={()=>setMenu("Contact")}><a href='#contact'>Contact</a>{menu==="Contact"?<hr/>:<></>}</li>
-              <button><a href={resume} download="Resume">Download Resume</a></button>
             </ul> 
-            <p className='nav-btn' onClick={()=>setIsMobile(!isMobile)} >{isMobile?<MdMenu/>:<RxCross2/>} </p> 
+            <p className='nav-btn' onClick={()=>setIsMobile(!isMobile)} >{isMobile?<RxCross2/>: <MdMenu/>} </p> 
         </div> 
     </div>
   )
