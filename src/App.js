5dfc4dd90137b87fa1c11186b71592d0fa2b0962
kaviyathirumali <kaviyathiru007@gmail.com>
2024-04-22 22:28:44 +0530

    Updated website with changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes  } from "react-router-dom"; 
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Componets/Home/Home";
 import About from "./Componets/About/About";
 import Resume from "./Componets/Resume/Resume";
@@ -8,17 +9,16 @@ import Project from "./Componets/Project/Project";
 
 function App() {
   return (
-      <Router>
-        <Navbar />
-        <Routes>
-          {/*<Route exact path="/kaviyat.github.io" Component={Navlist}/>*/}
-          <Route exact path="/" Component={Home}/>
-          <Route exact path="/about" Component={About}/>
-          <Route exact path="/resume" Component={Resume}/>
-          <Route exact path="/project" Component={Project}/>
-          <Route exact path="/contact" Component={Contact}/>
-        </Routes>
-      </Router>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/kaviyat.github.io" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
