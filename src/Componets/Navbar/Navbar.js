b32133f8ea83d42a5edd109643986968d1bb6f5d
kaviyathirumali <kaviyathiru007@gmail.com>
2024-04-22 22:39:42 +0530

    Updated website with changes

--- src/Componets/Navbar/Navbar.js
+++ src/Componets/Navbar/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
     <div className='profile'>
         <div  className='navbar'>
             <ul className={isMobile?"nav-link-mobile":"nav-list"} onClick={()=>setIsMobile(false)}> 
-              <li onClick={()=>setMenu("Home")}><Link to='/'>Home</Link>{menu==="Home"?<hr/>:<></>}</li>
+              <li onClick={()=>setMenu("Home")}><Link to='/kaviyat.github.io'>Home</Link>{menu==="Home"?<hr/>:<></>}</li>
               <li onClick={()=>setMenu("About")}><Link to='/about'>About</Link>{menu==="About"?<hr/>:<></>}</li>
               <li onClick={()=>setMenu("Resume")}><Link to='/resume'>Resume</Link>{menu==="Resume"?<hr/>:<></>}</li>
               <li onClick={()=>setMenu("Project")}><Link to='/project'>Project</Link>{menu==="Project"?<hr/>:<></>}</li>
